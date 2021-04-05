82af8f40f94a8f0dc3642b2c2bbacc1d03c232d3
Michal Franc <michal.franc@form3.tech>
2021-04-05 15:19:26 +0100

    by default run `start` command

--- cmd/rgt/main.go
+++ cmd/rgt/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"github.com/michal-franc/rgt/internal/app/rgt/commands"
+	"os"
 )
 
+const DefaultCommand = "start"
+
 func main() {
+	// if no command specified add in default command
+	if len(os.Args) <= 1 {
+		os.Args = append(os.Args, DefaultCommand)
+	}
 	commands.Execute()
 }
